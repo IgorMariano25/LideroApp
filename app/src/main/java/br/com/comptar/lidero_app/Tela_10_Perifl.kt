f3dc5e261a7119eeb9fa944e4dee42d4e6f660ed
Igor Mariano <igor-mariano@outlook.com>
2023-05-27 15:11:23 -0300

    Criando o arquivo Tela_10_Perfil.kt

---
+++ app/src/main/java/br/com/comptar/lidero_app/Tela_10_Perifl.kt
@@ -0,0 +1,12 @@
+package br.com.comptar.lidero_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Tela_10_Perifl : AppCompatActivity (){
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+        }
+    }
+}
