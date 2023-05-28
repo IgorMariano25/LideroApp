6f5f2742df364c9968228b3f7b1a20c0cf52155f
Igor Mariano <igor-mariano@outlook.com>
2023-05-28 12:30:48 -0300

    Renomenado arquivo e nome da classe

--- app/src/main/java/br/com/comptar/lidero_app/Tela10Perifl.kt
+++ app/src/main/java/br/com/comptar/lidero_app/Tela10Perifl.kt
@@ -3,7 +3,7 @@ package br.com.comptar.lidero_app
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class Tela_10_Perifl : AppCompatActivity (){
+class Tela10Perifl : AppCompatActivity (){
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             supportActionBar?.hide()
