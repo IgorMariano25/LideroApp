fa7186ec49b01e2452707ab36e4cd045677b6a48
DeveSerOGui <guifelixs@gmail.com>
2023-05-30 21:43:20 -0300

    Pagina de pergunta 4 finalizada

---
+++ app/src/main/java/br/com/comptar/lidero_app/Pergunta3Activity.kt
@@ -0,0 +1,11 @@
+package br.com.comptar.lidero_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Pergunta3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pergunta3)
+    }
+}
