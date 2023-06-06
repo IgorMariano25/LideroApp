ecd973e52fc126ee837e0c850b88c0e16c15640b
Igor Mariano <igor-mariano@outlook.com>
2023-06-06 16:39:12 -0300

    Adicionando intent para mudar as telas de pergunta

--- app/src/main/java/br/com/comptar/lidero_app/Pergunta4Activity.kt
+++ app/src/main/java/br/com/comptar/lidero_app/Pergunta4Activity.kt
@@ -1,11 +1,20 @@
 package br.com.comptar.lidero_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Pergunta4Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pergunta4)
+
+        val btnActivityPergunta1 = findViewById<Button>(R.id.btnConfResp)
+        btnActivityPergunta1.setOnClickListener {
+            //Exemplo de Intent Explicita
+            val intent = Intent(this@Pergunta4Activity, Pergunta5Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
