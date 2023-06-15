699e583d21945e434c3d7f91671cd2ab29af8328
GuiFelix <guifelixs@gmail.com>
2023-06-15 13:51:25 -0300

    Adicionando troca de cores nas perguntas 2 e 3

--- app/src/main/java/br/com/comptar/lidero_app/Pergunta3Activity.kt
+++ app/src/main/java/br/com/comptar/lidero_app/Pergunta3Activity.kt
@@ -1,14 +1,42 @@
 package br.com.comptar.lidero_app
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Pergunta3Activity : AppCompatActivity() {
+    private var selectedButton: View? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pergunta3)
+
+        val btnDiscTotal1 = findViewById<Button>(R.id.btnDiscTotal)
+        btnDiscTotal1.setOnClickListener {
+            selectButton(btnDiscTotal1)
+        }
+
+        val btnDisc1 = findViewById<Button>(R.id.btnDisc)
+        btnDisc1.setOnClickListener {
+            selectButton(btnDiscTotal1)
+        }
+
+        val btnNem = findViewById<Button>(R.id.btnNemDiscNemCon)
+        btnNem.setOnClickListener {
+            selectButton(btnDiscTotal1)
+        }
+
+        val btnCon1 = findViewById<Button>(R.id.btnConcord)
+        btnCon1.setOnClickListener {
+            selectButton(btnDiscTotal1)
+        }
+
+        val btnConTotal1 = findViewById<Button>(R.id.btnConcorTotal)
+        btnConTotal1 .setOnClickListener {
+            selectButton(btnDiscTotal1)
+        }
 
         val btnActivityPergunta1 = findViewById<Button>(R.id.btnConfResp)
         btnActivityPergunta1.setOnClickListener {
@@ -16,5 +44,13 @@ class Pergunta3Activity : AppCompatActivity() {
             val intent = Intent(this@Pergunta3Activity, Pergunta4Activity::class.java)
             startActivity(intent)
         }
+    }
+    private fun selectButton(button: Button) {
+        selectedButton?.apply {
+            setBackgroundColor(Color.WHITE)
+        }
+
+        button.setBackgroundColor(Color.rgb(153,149,149))
+        selectedButton = button
     }
 }
