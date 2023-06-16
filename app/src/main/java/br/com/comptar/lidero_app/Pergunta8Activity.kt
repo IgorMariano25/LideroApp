d618414e1479eadcf0e7809d6906ddaebf732809
Igor Mariano <igor-mariano@outlook.com>
2023-06-16 18:59:37 -0300

    Adicionando função selectButton

--- app/src/main/java/br/com/comptar/lidero_app/Pergunta8Activity.kt
+++ app/src/main/java/br/com/comptar/lidero_app/Pergunta8Activity.kt
@@ -28,9 +28,16 @@ class Pergunta8Activity : AppCompatActivity() {
         btnConfirmaRespostaActivityPergunta1.isEnabled = true
     }
 
-   private fun DesabilitarButtonConfirmarRespoasta() {
-       btnConfResp.setTextColor(Color.WHITE)
-       btnConfResp.setBackgroundColor(cor_cinza_btn_desabilitado)
-       btnConfResp.isEnabled = false
-   }
+    private fun DesabilitarButtonConfirmarResposta() {
+        btnConfirmaRespostaActivityPergunta1.setTextColor(Color.WHITE)
+        btnConfirmaRespostaActivityPergunta1.setBackgroundColor(cor_cinza_btn_desabilitado)
+        btnConfirmaRespostaActivityPergunta1.isEnabled = false
+    }
+
+    private fun selectButton(button: Button) {
+        selectedButton?.apply {
+            setBackgroundColor(Color.WHITE)
+        }
+        button.setBackgroundColor(cor_cinza_btn_clicado)
+    }
 }
