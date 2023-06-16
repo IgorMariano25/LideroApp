fd64687c47e26e227614e5bcd16dd26a46f6d076
Igor Mariano <igor-mariano@outlook.com>
2023-06-16 17:37:24 -0300

    Alterando nome da variável

--- app/src/main/java/br/com/comptar/lidero_app/Pergunta2Activity.kt
+++ app/src/main/java/br/com/comptar/lidero_app/Pergunta2Activity.kt
@@ -9,11 +9,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.startActivity
 
 class Pergunta2Activity : AppCompatActivity() {
+
+    private lateinit var  btnConfirmaRespostabActivityPergunta2 : Button
+    private var selectedButton: View? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pergunta2)
 
-        val btnActivityPergunta1 = findViewById<Button>(R.id.btnConfResp)
+        btnActivityPergunta2 = findViewById<Button>(R.id.btnConfResp)
         btnActivityPergunta1.setOnClickListener {
             //Exemplo de Intent Explicita
             val intent = Intent(this@Pergunta2Activity, Pergunta3Activity::class.java)
@@ -23,14 +26,14 @@ class Pergunta2Activity : AppCompatActivity() {
 
     //Função que habilita o botão de confirmar resposta quando alguma opção é selecionada
     private fun HabiltarButtonConfirmarResposta() {
-        btnConfResp.setTextColor(Color.WHITE)
-        btnConfResp.setBackgroundColor(cor_verde_btn_habilitado)
-        btnConfResp.isEnabled = true
+        btnActivityPergunta2.setTextColor(Color.WHITE)
+        btnActivityPergunta2.setBackgroundColor(cor_verde_btn_habilitado)
+        btnActivityPergunta2.isEnabled = true
     }
 
     private fun DesabilitarButtonConfirmarRespoasta() {
-        btnConfResp.setTextColor(Color.WHITE)
-        btnConfResp.setBackgroundColor(cor_cinza_btn_desabilitado)
-        btnConfResp.isEnabled = false
+        btnActivityPergunta2.setTextColor(Color.WHITE)
+        btnActivityPergunta2.setBackgroundColor(cor_cinza_btn_desabilitado)
+        btnActivityPergunta2.isEnabled = false
     }
 }
