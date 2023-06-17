1e3429aa115ee45a568f163c7385d1b8cec8b6ec
Felipe Castelhano <felipcastelhanogh@gmail.com>
2023-06-17 16:09:31 -0300

    Implementando logica das perguntas

--- app/src/main/java/br/com/comptar/lidero_app/ResultadoPerfil.kt
+++ app/src/main/java/br/com/comptar/lidero_app/ResultadoPerfil.kt
@@ -16,28 +16,41 @@ class ResultadoPerfil : AppCompatActivity() {
 
 
     }
+
     private fun exibirResultadoFinal() {
 
-        val tvresultado = findViewById<TextView>(R.id.textView7)
-        tvresultado.text = SharedData.gerador.toString()
-
-    }
-
-    private fun EncontraPerfilUsuario(){
         var maior = SharedData.gerador
+        var perfil : String = "gerador"
+        var trilha : String = "Trilha Gerador"
 
         //Verifique se as outras variáveis são maiores do que o valor atual de 'maior'
         if (SharedData.organizador > maior) {
             maior = SharedData.organizador
+            perfil = "Organizador"
+            trilha = "Trilha Organizador"
+
         }
 
         if (SharedData.fazedor > maior) {
             maior = SharedData.fazedor
+            perfil = "Fazedor"
+            trilha ="Trilha Fazedor"
         }
 
         if (SharedData.conector > maior) {
             maior = SharedData.conector
+            perfil ="Conector"
+            trilha = "trilha Conector"
         }
+
+        val tvPerfil = findViewById<TextView>(R.id.textView2)
+        tvPerfil.text = "De acordo com as informções fornecidas, identificamos que o seu perfil é ${perfil} e a trilha que melhor atende a sua demanda de carreira é:"
+
+        val tvTrilha = findViewById<TextView>(R.id.textView7)
+        tvTrilha.text = trilha
+
+
     }
+
 }
 
