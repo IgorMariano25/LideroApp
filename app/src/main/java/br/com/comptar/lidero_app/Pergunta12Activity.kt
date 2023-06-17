1dc527d03c65e7515c20ac8bc09aeaadf6983576
Igor Mariano <igor-mariano@outlook.com>
2023-06-17 10:06:25 -0300

    Ajustando função DesabilitarButtonConfirmarResposta

--- app/src/main/java/br/com/comptar/lidero_app/Pergunta12Activity.kt
+++ app/src/main/java/br/com/comptar/lidero_app/Pergunta12Activity.kt
@@ -39,9 +39,9 @@ class Pergunta12Activity : AppCompatActivity() {
         btnConfirmaRespostaActivityPergunta1.isEnabled = true
     }
 
-    private fun DesabilitarButtonConfirmarRespoasta() {
-        btnConfResp.setTextColor(Color.WHITE)
-        btnConfResp.setBackgroundColor(cor_cinza_btn_desabilitado)
-        btnConfResp.isEnabled = false
+    private fun DesabilitarButtonConfirmarResposta() {
+        btnConfirmaRespostaActivityPergunta1.setTextColor(Color.WHITE)
+        btnConfirmaRespostaActivityPergunta1.setBackgroundColor(cor_cinza_btn_desabilitado)
+        btnConfirmaRespostaActivityPergunta1.isEnabled = false
     }
 }
