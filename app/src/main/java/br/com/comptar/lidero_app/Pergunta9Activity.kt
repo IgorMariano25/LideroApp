2ba6255b7ed4e40968051a222e528046810289db
Igor Mariano <igor-mariano@outlook.com>
2023-06-17 09:52:17 -0300

    Adicionando core que mudam de acordo com o estado para o button

--- app/src/main/java/br/com/comptar/lidero_app/Pergunta9Activity.kt
+++ app/src/main/java/br/com/comptar/lidero_app/Pergunta9Activity.kt
@@ -13,6 +13,13 @@ class Pergunta9Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pergunta9)
 
+    //Criando cor para o botão confirmar resposta desabilitado
+    val cor_cinza_btn_desabilitado = Color.rgb(203,203,203)
+    //Criando cor para o botão confirmar resposta habilitado
+    val cor_carbon_btn_habilitado = Color.rgb(51,51,51)
+    // Criando uma nova cor para adicionar ao botão de respostas
+    val cor_cinza_btn_clicado = Color.rgb(97, 97, 97)
+
         val btnActivityPergunta1 = findViewById<Button>(R.id.btnConfResp)
         btnActivityPergunta1.setOnClickListener {
             //Exemplo de Intent Explicita
