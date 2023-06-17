c9dfa7977da3ff2961d499695db10fcc0468637d
Igor Mariano <igor-mariano@outlook.com>
2023-06-17 09:59:48 -0300

    Adicionando import android.view.View

--- app/src/main/java/br/com/comptar/lidero_app/Pergunta11Activity.kt
+++ app/src/main/java/br/com/comptar/lidero_app/Pergunta11Activity.kt
@@ -3,6 +3,7 @@ package br.com.comptar.lidero_app
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.startActivity
