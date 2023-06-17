2a3511b441dafca59431da93033e06d5c811d417
Igor Mariano <igor-mariano@outlook.com>
2023-06-17 19:22:18 -0300

    Adicionando intent para pular as perguntas do quiz Pergunta12Activity

--- app/src/main/java/br/com/comptar/lidero_app/Pergunta12Activity.kt
+++ app/src/main/java/br/com/comptar/lidero_app/Pergunta12Activity.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.startActivity
 
@@ -28,6 +29,12 @@ class Pergunta12Activity : AppCompatActivity() {
         btnConfirmaRespostaActivityPergunta12.setOnClickListener {
             //Exemplo de Intent Explicita
             val intent = Intent(this@Pergunta12Activity, ResultadoPerfil::class.java)
+            startActivity(intent)
+        }
+
+        val pularQuiz = findViewById<ImageButton>(R.id.btnExit)
+        pularQuiz.setOnClickListener{
+            val intent = Intent(this@Pergunta12Activity, Tela10Perifl::class.java)
             startActivity(intent)
         }
 
