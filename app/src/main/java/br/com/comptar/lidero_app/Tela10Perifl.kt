513925b1b60b4f47fae313d76e3313dfc5b5c858
Igor Mariano <igor-mariano@outlook.com>
2023-06-17 18:56:54 -0300

    Resultado de perfil e trilha quando o usuário clica no X para pular as perguntas

--- app/src/main/java/br/com/comptar/lidero_app/Tela10Perifl.kt
+++ app/src/main/java/br/com/comptar/lidero_app/Tela10Perifl.kt
@@ -1,6 +1,8 @@
 package br.com.comptar.lidero_app
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class Tela10Perifl : AppCompatActivity (){
@@ -8,5 +10,48 @@ class Tela10Perifl : AppCompatActivity (){
             super.onCreate(savedInstanceState)
             supportActionBar?.hide()
             setContentView(R.layout.activity_tela_10_perfil)
+            exibirResultadoFinal()
         }
+    private fun exibirResultadoFinal() {
+        var maior = SharedData.gerador
+        var perfil : String = "Gerador"
+        var trilhaRecomendada : String = "Rumo ao crescimento \uD83C\uDD99☝️"
+
+        //Verifique se as outras variáveis são maiores do que o valor atual de 'maior'
+        if (SharedData.organizador > maior) {
+            maior = SharedData.organizador
+            perfil = "Organizador"
+            trilhaRecomendada = "Organizando seus pontos fortes \uD83D\uDCAA"
+        }
+        else{
+            perfil = "Quiz não foi realizado \uD83D\uDE22"
+            trilhaRecomendada = "Quiz não foi realizado \uD83D\uDE22"
+        }
+
+        if (SharedData.fazedor > maior) {
+            maior = SharedData.fazedor
+            perfil = "Fazedor"
+            trilhaRecomendada ="Chegando com tudo \uD83D\uDE80"
+        }
+        else{
+            perfil = "Quiz não foi realizado \uD83D\uDE22"
+            trilhaRecomendada = "Quiz não foi realizado \uD83D\uDE22"
+        }
+
+        if (SharedData.conector > maior) {
+            maior = SharedData.conector
+            perfil ="Conector"
+            trilhaRecomendada = "Construindo seu caminho \uD83E\uDDF1"
+        }
+        else{
+            perfil = "Quiz não foi realizado \uD83D\uDE22"
+            trilhaRecomendada = "Quiz não foi realizado \uD83D\uDE22"
+        }
+
+        val tvResultadoPerfil = findViewById<TextView>(R.id.tvResultadoPerfil)
+        tvResultadoPerfil.text = "${perfil}"
+
+        val tvResultadoTrilha = findViewById<TextView>(R.id.tvResultadoTrilhaRecomendada)
+        tvResultadoTrilha.text = trilhaRecomendada
+    }
 }
