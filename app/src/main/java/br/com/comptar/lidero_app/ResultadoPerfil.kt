242b05f814c24fdbc2f02b94e6f2d065bf9be764
Igor Mariano <igor-mariano@outlook.com>
2023-06-17 18:54:35 -0300

    Exibindo resultado com nome colorido e modificando textos de trilhas recomendadas

--- app/src/main/java/br/com/comptar/lidero_app/ResultadoPerfil.kt
+++ app/src/main/java/br/com/comptar/lidero_app/ResultadoPerfil.kt
@@ -1,7 +1,11 @@
 package br.com.comptar.lidero_app
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -29,40 +33,48 @@ class ResultadoPerfil : AppCompatActivity() {
         val ImageView = findViewById<ImageView>(R.id.imageView6)
         var maior = SharedData.gerador
         var perfil : String = "gerador"
-        var trilha : String = "Trilha Gerador"
+        var trilhaRecomendada : String = "Rumo ao crescimento \uD83C\uDD99☝️"
 
         //Verifique se as outras variáveis são maiores do que o valor atual de 'maior'
         if (SharedData.organizador > maior) {
             maior = SharedData.organizador
             perfil = "Organizador"
-            trilha = "Trilha Organizador"
+            trilhaRecomendada = "Organizando seus pontos fortes \uD83D\uDCAA"
             ImageView.setImageResource(R.drawable.vetor_perfil_organizador)
-
-
         }
 
         if (SharedData.fazedor > maior) {
             maior = SharedData.fazedor
             perfil = "Fazedor"
-            trilha ="Trilha Fazedor"
+            trilhaRecomendada ="Chegando com tudo \uD83D\uDE80"
             ImageView.setImageResource(R.drawable.vetor_perfil_fazedor)
         }
 
         if (SharedData.conector > maior) {
             maior = SharedData.conector
             perfil ="Conector"
-            trilha = "trilha Conector"
+            trilhaRecomendada = "Construindo seu caminho \uD83E\uDDF1"
             ImageView.setImageResource(R.drawable.vetor_perfil_conector)
         }
 
         val tvPerfil = findViewById<TextView>(R.id.textView2)
-        tvPerfil.text = "De acordo com as informções fornecidas, identificamos que o seu perfil é ${perfil} e a trilha que melhor atende a sua demanda de carreira é:"
+        val texto = "De acordo com as informações fornecidas, identificamos que o seu perfil é ${perfil} e a trilha que melhor atende a sua demanda de carreira é:"
+        val spannable = SpannableString(texto)
+
+        // Define a cor que deseja aplicar à palavra ${perfil}
+        val cor = Color.parseColor("#FF6600")
+
+        // Encontra a posição da palavra ${perfil} no texto
+        val posicaoInicio = texto.indexOf("${perfil}")
+        val posicaoFim = posicaoInicio + "${perfil}".length
+
+        // Aplica a cor à palavra ${perfil}
+        spannable.setSpan(ForegroundColorSpan(cor), posicaoInicio, posicaoFim, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        // Define o texto formatado no TextView
+        tvPerfil.text = spannable
 
         val tvTrilha = findViewById<TextView>(R.id.textView7)
-        tvTrilha.text = trilha
-
-
+        tvTrilha.text = trilhaRecomendada
     }
-
 }
-
