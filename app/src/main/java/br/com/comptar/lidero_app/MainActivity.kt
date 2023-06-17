c33c01be1d1f1af0537ebd094e2098c9570850c6
Felipe Castelhano <felipcastelhanogh@gmail.com>
2023-06-17 16:41:06 -0300

    Add intent tela main (cadastro e home)

--- app/src/main/java/br/com/comptar/lidero_app/MainActivity.kt
+++ app/src/main/java/br/com/comptar/lidero_app/MainActivity.kt
@@ -1,11 +1,26 @@
 package br.com.comptar.lidero_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnEntrar = findViewById<Button>(R.id.btnEntrar1)
+        btnEntrar .setOnClickListener {
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        val btnCadastrese = findViewById<Button>(R.id.btnCadastro)
+        btnCadastrese.setOnClickListener{
+            val intent = Intent(this@MainActivity, TelaCadastro::class.java)
+            startActivity(intent)
+        }
     }
 }
