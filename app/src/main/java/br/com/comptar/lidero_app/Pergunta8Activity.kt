0928134943731070a9638d2e135284d85707c6a0
Igor Mariano <igor-mariano@outlook.com>
2023-06-17 19:19:24 -0300

    Adicionando intent para pular as perguntas do quiz Pergunta8Activity

--- app/src/main/java/br/com/comptar/lidero_app/Pergunta8Activity.kt
+++ app/src/main/java/br/com/comptar/lidero_app/Pergunta8Activity.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.startActivity
 
@@ -28,6 +29,12 @@ class Pergunta8Activity : AppCompatActivity() {
         btnConfirmaRespostaActivityPergunta8.setOnClickListener {
             //Exemplo de Intent Explicita
             val intent = Intent(this@Pergunta8Activity, Pergunta9Activity::class.java)
+            startActivity(intent)
+        }
+
+        val pularQuiz = findViewById<ImageButton>(R.id.btnExit)
+        pularQuiz.setOnClickListener{
+            val intent = Intent(this@Pergunta8Activity, Tela10Perifl::class.java)
             startActivity(intent)
         }
 
