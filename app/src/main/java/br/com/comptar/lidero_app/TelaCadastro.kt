1f30167f37af74cc954699a915b0531b6d6267d9
Igor Mariano <igor-mariano@outlook.com>
2023-06-17 23:05:01 -0300

    Adicionando listener ao botão de criar conta

--- app/src/main/java/br/com/comptar/lidero_app/TelaCadastro.kt
+++ app/src/main/java/br/com/comptar/lidero_app/TelaCadastro.kt
@@ -1,11 +1,20 @@
 package br.com.comptar.lidero_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TelaCadastro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tela_cadastro)
+
+        // Alterando entre as telas ao clicar no botão de confirmar resposta
+        val btnConfirmarCadastro = findViewById<Button>(R.id.btnCriarConta)
+        btnConfirmarCadastro.setOnClickListener {
+            val intent = Intent(this@TelaCadastro, TelaTransicaoPerguntas::class.java)
+            startActivity(intent)
+        }
     }
 }
