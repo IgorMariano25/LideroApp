9782c0a220c1ec004c895277e651731b916972a4
DeveSerOGui <guifelixs@gmail.com>
2023-06-20 15:17:42 -0300

    FEAT: Adicionando navegação no footer de Cursos e alterando as ImageView para ImageButton

--- app/src/main/java/br/com/comptar/lidero_app/Tela11Cursos.kt
+++ app/src/main/java/br/com/comptar/lidero_app/Tela11Cursos.kt
@@ -1,6 +1,8 @@
 package br.com.comptar.lidero_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class Tela11Cursos : AppCompatActivity (){
@@ -8,5 +10,19 @@ class Tela11Cursos : AppCompatActivity (){
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_tela_11_cursos)
+
+        val btnGoToPerfil = findViewById<ImageButton>(R.id.ivVetorPerfil)
+        btnGoToPerfil.setOnClickListener {
+            //Exemplo de Intent Explicita
+            val intent = Intent(this@Tela11Cursos, Tela10Perifl::class.java)
+            startActivity(intent)
+        }
+
+        val btnGoToHome = findViewById<ImageButton>(R.id.ivVetorCursos)
+        btnGoToHome.setOnClickListener {
+            //Exemplo de Intent Explicita
+            val intent = Intent(this@Tela11Cursos, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
